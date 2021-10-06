676be819377134b4d305bfdbb162b19b2879a4e4
ModLso <88741192+ModLso@users.noreply.github.com>
2021-10-06 21:13:46 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -511,7 +511,7 @@ async def play(_, message: Message):
                     InlineKeyboardButton("عرض القائمه.. 💕", callback_data="menu"),
                     InlineKeyboardButton("اخفاء القائمه.. 💕", callback_data="cls"),
                 ],[
-                    InlineKeyboardButton("💕 قناه السورس", url=f"https://t.me/so_alfaa")
+                    InlineKeyboardButton("💕 قناه السورس", url=f"https://t.me/")
                 ],
             ]
         )
@@ -639,7 +639,7 @@ async def play(_, message: Message):
                     InlineKeyboardButton("عرض القائمه..💕", callback_data="menu"),
                     InlineKeyboardButton("اخفاء القائمه..💕", callback_data="cls"),
                 ],[
-                    InlineKeyboardButton("💕 قناه السورس", url=f"https://t.me/so_alfaa") 
+                    InlineKeyboardButton("💕 قناه السورس", url=f"https://t.me/") 
                 ],
             ]
             )
@@ -738,7 +738,7 @@ async def lol_cb(b, cb):
                     InlineKeyboardButton("عرض القائمه..💕", callback_data="menu"),
                     InlineKeyboardButton("اخفاء القائمه..💕", callback_data="cls"),
                 ],[
-                    InlineKeyboardButton("💕 قناه السورس", url=f"https://t.me/so_alfaa")
+                    InlineKeyboardButton("💕 قناه السورس", url=f"https://t.me/")
                 ],
             ]
     )
